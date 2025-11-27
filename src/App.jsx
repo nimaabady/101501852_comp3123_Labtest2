94527b88a2ba3e16b1f1679f2503ce6b7814ba4c
Nima Abady <nima.abady@gmail.com>
2025-11-27 16:48:08 -0500

    name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Weather from './components/weather'
+import Weather from './components/Weather'
 import './App.css'
 
 function App() {
